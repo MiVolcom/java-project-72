773d0c93d772a96431fa976e05e06d662b48f508
Olga Molkova <olgamolkova190719@gmail.com>
2024-10-16 17:50:34 +0300

    fix create()

--- app/src/main/java/hexlet/code/util/NamedRoutes.java
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -1,6 +1,9 @@
 package hexlet.code.util;
 
 public class NamedRoutes {
+    public static String rootPath() {
+        return "/";
+    }
     public static String urlsPath() {
         return "/urls";
     }
